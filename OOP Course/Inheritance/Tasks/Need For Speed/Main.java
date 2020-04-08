46a38bb778d9d07294156263fe4a72885c6b486f
Valentin Vasilev <valentin7969@gmail.com>
2020-04-08 12:13:41 +0300

    Create Main.java

---
+++ OOP Course/Inheritance/Tasks/Need For Speed/Main.java
@@ -0,0 +1,10 @@
+package needForSpeed;
+
+public class Main {
+    public static void main(String[] args) {
+
+        SportCar sport = new SportCar(10,1);
+        System.out.println(sport.getFuelConsumption());
+        
+    }
+}
