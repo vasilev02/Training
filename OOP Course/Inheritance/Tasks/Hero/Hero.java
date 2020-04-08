4db1f1463773eb443619af333436cd31f75872c6
Valentin Vasilev <valentin7969@gmail.com>
2020-04-08 12:18:31 +0300

    Add files via upload

---
+++ OOP Course/Inheritance/Tasks/Hero/Hero.java
@@ -0,0 +1,28 @@
+package hero;
+
+public class Hero {
+
+    protected String username;
+    protected int level;
+
+    public Hero(String username, int level) {
+        this.username = username;
+        this.level = level;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public int getLevel() {
+        return this.level;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Type: %s Username: %s Level: %s",
+                this.getClass().getName(),
+                this.getUsername(),
+                this.getLevel());
+    }
+}
