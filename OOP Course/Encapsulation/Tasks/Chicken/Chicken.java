b0ea32dfb964e9e77ed91cee04c0ac51bce1e4c0
Valentin Vasilev <valentin7969@gmail.com>
2020-04-08 12:43:43 +0300

    Add files via upload

---
+++ OOP Course/Encapsulation/Tasks/Chicken/Chicken.java
@@ -0,0 +1,46 @@
+package chicken;
+
+public class Chicken {
+
+    private String name;
+    private int age;
+
+    public Chicken(String name, int age) {
+        setName(name);
+        setAge(age);
+    }
+
+    private void setName(String name) {
+        if(name==null || name.trim().isEmpty()){
+            throw new IllegalArgumentException("Name cannot be empty.");
+        }
+        this.name = name;
+    }
+
+    private void setAge(int age) {
+        if(age<0 || age>15){
+            throw new IllegalArgumentException("Age should be between 0 and 15.");
+        }
+        this.age = age;
+    }
+
+    public double productPerDay(){
+        return calculateProductPerDay();
+    }
+
+    private double calculateProductPerDay(){
+        if(this.age<6){
+            return 2.0;
+        }else if(this.age<12){
+            return 1.0;
+        }else{
+            return 0.75;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Chicken %s (age %d) can produce %.2f eggs per day.",
+                this.name,this.age,productPerDay());
+    }
+}
