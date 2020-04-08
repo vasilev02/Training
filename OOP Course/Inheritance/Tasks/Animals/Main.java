27ad48a847e3bec7965abaa9cf318575f7fbe419
Valentin Vasilev <valentin7969@gmail.com>
2020-04-08 12:21:20 +0300

    Create Main.java

---
+++ OOP Course/Inheritance/Tasks/Animals/Main.java
@@ -0,0 +1,11 @@
+package animals;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Dog dog = new Dog("Doggy", 24, "fm");
+
+        System.out.println(dog.toString());
+
+    }
+}
