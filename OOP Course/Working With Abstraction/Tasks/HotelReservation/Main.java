55c3531a84cc1cce80d1afefb74c8ff147b9f328
Valentin Vasilev <valentin7969@gmail.com>
2020-04-08 12:06:05 +0300

    Update Main.java

--- OOP Course/Working With Abstraction/Tasks/HotelReservation/Main.java
+++ OOP Course/Working With Abstraction/Tasks/HotelReservation/Main.java
@@ -1,1 +1,22 @@
+package hotelReservation;
 
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String[] info = scanner.nextLine().split("\\s+");
+
+        double price = Double.parseDouble(info[0]);
+        int numberOfDays = Integer.parseInt(info[1]);
+        String nameOfSeason = info[2];
+        String type = info[3];
+
+        Reservation reservation = new Reservation(price,numberOfDays,Season.valueOf(nameOfSeason),Discount.valueOf(type));
+
+        System.out.printf("%.2f",PriceCalculator.getPrice(reservation));
+
+    }
+}
