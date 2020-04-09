b73068e1b4bfa2f2959a537945a1fb537a5aca94
Valentin Vasilev <valentin7969@gmail.com>
2020-04-09 09:42:46 +0300

    Add files via upload

---
+++ OOP Course/Polymorphism/Tasks/Food.java
@@ -0,0 +1,16 @@
+package wildFarm;
+
+public abstract class Food {
+
+    private int quantity;
+
+    public Food(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getQuantity() {
+        return this.quantity;
+    }
+
+
+}
