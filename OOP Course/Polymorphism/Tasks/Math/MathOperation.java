b34c173cea58ab6af118885b1ddb486a3a8e0149
Valentin Vasilev <valentin7969@gmail.com>
2020-04-09 09:49:36 +0300

    Add files via upload

---
+++ OOP Course/Polymorphism/Tasks/Math/MathOperation.java
@@ -0,0 +1,17 @@
+package math;
+
+public class MathOperation {
+
+    public int add(int a , int b){
+        return a+b;
+    }
+
+    public int add(int a,int b,int c){
+        return this.add(a,b)+c;
+    }
+
+    public int add(int a,int b,int c,int d){
+        return this.add(a,b,c)+d;
+    }
+
+}
