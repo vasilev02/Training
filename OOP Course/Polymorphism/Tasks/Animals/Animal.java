6b36e12a1122abfc77e2e0f5112bfb6a2874bf7a
Valentin Vasilev <valentin7969@gmail.com>
2020-04-09 09:53:23 +0300

    Add files via upload

---
+++ OOP Course/Polymorphism/Tasks/Animals/Animal.java
@@ -0,0 +1,19 @@
+package animals;
+
+public abstract class Animal {
+
+    private String name;
+    private String favouriteFood;
+
+    public Animal(String name, String favouriteFood) {
+        this.name = name;
+        this.favouriteFood = favouriteFood;
+    }
+
+    public String explainSelf(){
+
+        return String.format("I am %s and my favourite food is %s",this.name,this.favouriteFood);
+
+    }
+
+}
